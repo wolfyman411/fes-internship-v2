4e27a43f8f9a17fec03ba22fb107e700797515c4
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-07-08 16:25:24 -0400

    partial time keeping

--- app/player/[id]/components/AudioPlayer.tsx
+++ app/player/[id]/components/AudioPlayer.tsx
@@ -2,17 +2,39 @@
 
 import { faPause, faPlay, faRotateLeft, faRotateRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 export default function AudioPlayer({book = {} as Book}) {
 
   const [audio,setAudio] = useState(() => new Audio())
   const [audioPlaying,setAudioPlaying] = useState(true)
   const [loaded,setLoaded] = useState(false)
+  const [animationFrame,setAnimationFrame] = useState(0)
+  const currentTime = useRef(0)
 
   useEffect(() => {
     getAudio()
   },[])
+
+  useEffect(() => {
+    if (audioPlaying) {
+        setAnimationFrame(requestAnimationFrame(timerCount))
+    }
+    else {
+        cancelAnimationFrame(animationFrame)
+    }
+  },[audioPlaying])
+
+  function timerCount() {
+    const playElement:HTMLAudioElement|null = document.querySelector(".book__audio")
+
+    currentTime.current = playElement?.currentTime || 0
+    if (audioPlaying) {
+        setAnimationFrame(requestAnimationFrame(timerCount))
+    }
+    console.log(currentTime.current)
+  }
+
 
   async function getAudio() {
     setLoaded(false)
@@ -74,7 +96,7 @@ export default function AudioPlayer({book = {} as Book}) {
             </div>
         </div>
         <div className="audio__progress--wrapper">
-            <div className="audio__time">00:00</div>
+            <div className="audio__time">{currentTime.current}</div>
             <input type="range" className="audio__progress--bar" defaultValue={0} max={100} style={{background:"linear-gradient(to right, rgb(43, 217, 124) 0%, rgb(109, 120, 125) 0%)"}}/>
             <div className="audio__time">{`${(Math.floor(audio.duration/60)).toString().padStart(2,"0")}:${(Math.floor(audio.duration%60)).toString().padStart(2,"0")}`}</div>
         </div>
