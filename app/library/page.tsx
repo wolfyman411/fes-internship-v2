a460143500dbcaa21b57282e7fd2ccbed2eebe27
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-07-09 11:34:55 -0400

    improved library display

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -19,16 +19,30 @@ export default function page() {
         <div>
           <div className="for-you__title">Saved Books</div>
           <div className="for-you__sub--title">{user.savedBooks.length}
-            {user.savedBooks.length > 1 ? " items" : " item"}
+            {user.savedBooks.length > 1 || user.savedBooks.length <= 0 ? " items" : " item"}
           </div>
-          <BooksDisplay dataArray={user.savedBooks}/>
+          {user.savedBooks.length > 0 ? (
+            <BooksDisplay dataArray={user.savedBooks}/>
+          ) : (
+            <div className="finished__books--block-wrapper">
+              <div className="finished__books--title">Save your favorite books!</div>
+              <div className="finished__books--sub-title">When you save a book, it will appear here.</div>
+            </div>
+          )}
         </div>
         <div>
           <div className="for-you__title">Finished</div>
           <div className="for-you__sub--title">{user.finishedBooks.length}
-            {user.finishedBooks.length > 1 ? " items" : " item"}
+            {user.finishedBooks.length > 1 || user.finishedBooks.length <= 0  ? " items" : " item"}
           </div>
-          <BooksDisplay dataArray={user.finishedBooks}/>
+          {user.finishedBooks.length > 0 ? (
+            <BooksDisplay dataArray={user.finishedBooks}/>
+          ) : (
+            <div className="finished__books--block-wrapper">
+              <div className="finished__books--title">Done and dusted!</div>
+              <div className="finished__books--sub-title">When you finish a book, you can find it here later.</div>
+            </div>
+          )}
         </div>
       </div>
     </div>
